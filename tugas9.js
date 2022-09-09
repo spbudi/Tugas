0774a940204ac6097d712a07698f144f35e0058e
spbudi <setyopambudi34@gmail.com>
2022-09-09 21:57:24 +0700

    spiral

---
+++ tugas9.js
@@ -0,0 +1,32 @@
+function spiral(param1) {
+  let list = [];
+
+  let arr = [];
+  for (let i = 0; i < param1; i++) { 
+      arr[i] = [] // 
+      for (let j = 0; j < param1; j++) {
+          arr[i][j] = (i * param1) + j
+      }
+    }
+
+  while (arr.length > 1) {
+      // kanan
+      list = list.concat(arr.splice(0, 1)[0]);
+      // bawah
+      for (let idx in arr) {
+          list.push(arr[idx].splice(-1)[0]);
+      }
+      // kiri
+      list = list.concat(arr.splice(-1, 1)[0].reverse());
+      // atas
+      for (let idx = arr.length - 1; idx >= 0; idx--) {
+          list.push(arr[idx].splice(0, 1)[0]);
+      }
+  } if (arr.length > 0) {
+      list.push(arr.pop()[0]);
+  }
+  console.log(list)
+}
+spiral(5)
+spiral(6)
+spiral(7)
