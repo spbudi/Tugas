1924682ef464b415161c5cf3b765dcd521561b63
spbudi <setyopambudi34@gmail.com>
2022-09-12 10:11:43 +0700

    nodejs

--- tugas11.js
+++ tugas11.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 const readline = require('readline');
 
 const file = fs.readFileSync('data.json');
@@ -10,25 +10,25 @@ const rl = readline.createInterface({
   prompt: 'Tebakan: ',
 });
 
-let count = 0;
 console.log(
   'Selamat datang di permainan Tebak Kata, silakan isi dengan jawaban yang benar!\n'
 );
-console.log(`Pertanyaan: ${data[count].definition}`); // jokowi
+let count = 0;
+console.log(`Pertanyaan: ${data[count].definition}`);
 rl.prompt();
 
-rl.on('line', input => {
+rl.on('line', (input) => {
   if (input.toLowerCase() == data[count].term) {
     count++;
     if (count == data.length) {
-      console.log('Selamat Anda Benar\n');
-      console.log('Hore Anda Menang');
+      console.log('Selamat Anda Benar!\n');
+      console.log('Hore Anda Menang!');
       process.exit(0);
     }
     console.log('Selamat Anda Benar!\n');
-    console.log(`Pertanyaan: ${data[count].definition}`); // 1945
+    console.log(`Pertanyaan: ${data[count].definition}`);
   } else {
-    console.log('Wkwkwkwk, Anda kurang beruntung');
+    console.log('Wkwkwkwk, Anda kurang beruntung!');
   }
   rl.prompt();
 });
